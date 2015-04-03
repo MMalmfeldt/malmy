2574f3f3d0f0bd99ed1e51715eb90666a3a08cf2
Michael Malmfeldt <michael.malmfeldt@gmail.com>
2015-04-03 14:03:31 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Malmy::Application.config.secret_key_base = '79d1804b3ad5a5e4df8833ba1fa5f7201038c060288d4f031962431d20b8fe765a024c081511c1fe0838c5379b7d74cfe3cfdeb9f73fcbe7144e3fdc6f495fe8'
