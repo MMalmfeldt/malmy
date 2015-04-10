21c7ed845644ee9fa4b537a15b5270b7cf878000
Michael Malmfeldt <michael.malmfeldt@gmail.com>
2015-04-10 19:57:50 +0000

    contribute form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Malmy::Application.routes.draw do
   root 'quotes#index'
+
+  resources :quotes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
